d9f2cd721d4b59ef495e15f20a52c53aabfe55fe
Grant Powell <email@grantjamespowell.com>
2021-10-01 10:18:24 -0400

    Starting to implement tic tac toe under 'Play'

--- games/tic_tac_toe/src/lib.rs
+++ games/tic_tac_toe/src/lib.rs
@@ -1,8 +1,71 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use lib_table_top_core::{Play, Player, View};
+use thiserror::Error;
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Row {
+    Row0 = 0,
+    Row1 = 1,
+    Row2 = 2,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Col {
+    Col0 = 0,
+    Col1 = 1,
+    Col2 = 2,
+}
+
+/// A type representing a position on the board, denoted in terms of (x, y)
+pub type Position = (Col, Row);
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Board([[Option<Player>; 3]; 3]);
+
+use Col::*;
+use Row::*;
+
+pub struct Action(Row, Col);
+
+pub type Settings = [Player; 2];
+
+#[derive(Error, Clone, Debug, PartialEq, Eq)]
+pub enum SettingsError {
+    /// Returned when both players are the same
+    #[error("Players must be different")]
+    PlayersCantBeTheSame,
+}
+
+#[derive(Error, Clone, Debug, PartialEq, Eq)]
+pub enum ActionError {
+    /// Returned when trying to claim an already claimed space
+    #[error("space ({:?}, {:?}) is taken", attempted.0, attempted.1)]
+    SpaceIsTaken { attempted: Position },
+    /// Returned when the wrong player tries to take a turn
+    #[error("not {:?}'s turn", attempted)]
+    OtherPlayerTurn { attempted: Player },
+}
+
+pub struct SpectatorView(Board);
+
+impl View for SpectatorView {
+    type Update = Action;
+
+    fn update(&mut self, action: Self::Update) {}
+}
+
+#[derive(Clone)]
+struct TicTacToe {
+    board: Board,
+}
+
+impl Play for TicTacToe {
+    type Action = Action;
+    type ActionError = ActionError;
+
+    type Settings = Settings;
+    type SettingsError = SettingsError;
+
+    type SpectatorView = SpectatorView;
 }
--- lib_table_top_core/src/lib.rs
+++ lib_table_top_core/src/lib.rs
@@ -1,15 +1,28 @@
 #![allow(dead_code)]
+#![feature(never_type)]
+#![feature(associated_type_defaults)]
 
-use std::sync::Arc;
 use rand::prelude::*;
 use rand_chacha::ChaCha20Rng;
+use std::sync::Arc;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Player(u16);
 
 pub trait View {
     type Update;
 
     fn update(&mut self, _update: Self::Update) {}
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct NoSecretPlayerInformation {}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct NoSecretPlayerInformationUpdate {}
+
+impl View for NoSecretPlayerInformation {
+    type Update = NoSecretPlayerInformationUpdate;
 }
 
 pub trait Play: Sized + Clone {
@@ -19,7 +32,7 @@ pub trait Play: Sized + Clone {
     type Settings;
     type SettingsError;
 
-    type PlayerView: View;
+    type PlayerView: View = NoSecretPlayerInformation;
     type SpectatorView: View;
 
     fn advance(
@@ -27,15 +40,11 @@ pub trait Play: Sized + Clone {
         settings: &Self::Settings,
         actions: &[(Player, Self::Action)],
         rng: &mut impl rand::Rng,
-    ) -> Result<
-        <<Self as Play>::SpectatorView as View>::Update,
-        Self::ActionError,
-    >;
+    ) -> Result<<<Self as Play>::SpectatorView as View>::Update, Self::ActionError>;
 
     fn action_requests(&self) -> Vec<Player>;
     fn player_view(&self) -> Self::PlayerView;
     fn spectator_view(&self) -> Self::SpectatorView;
-
 }
 
 struct GameRunner<T>
@@ -57,7 +66,7 @@ impl<T: Play> GameRunner<T> {
         let stream_num = self.history.len().try_into().unwrap();
         rng.set_stream(stream_num);
 
-        new_state.advance(&self.settings, actions, &mut rng);
+        let _ = new_state.advance(&self.settings, actions, &mut rng);
 
         todo!()
     }
