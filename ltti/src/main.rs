7afd9ad3807e56fceccd29dc81e7e3957d413d32
Grant Powell <email@grantjamespowell.com>
2021-10-05 00:30:24 -0400

    Read events from the terminal

--- ltti/src/main.rs
+++ ltti/src/main.rs
@@ -1,4 +1,5 @@
-use std::io;
+#![allow(unused_imports)]
+
 use tui::backend::CrosstermBackend;
 use tui::{Frame, Terminal};
 
@@ -8,11 +9,25 @@ use tui::symbols::DOT;
 use tui::text::{Span, Spans};
 use tui::widgets::{Block, Borders, List, ListItem, Tabs};
 
+use std::{io, sync::mpsc, thread, time::Duration};
+
+mod gui;
+
+use gui::tick::{background_terminal_events_and_ticks, Event::*};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let stdout = io::stdout();
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     terminal.clear()?;
+
+    let (events_sender, events_reciever) = mpsc::channel();
+    let tick_rate = Duration::from_millis(200);
+
+    thread::spawn(background_terminal_events_and_ticks(
+        tick_rate,
+        events_sender,
+    ));
 
     loop {
         terminal.draw(|rect| {
@@ -55,8 +70,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             rect.render_widget(tabs, chunks[0]);
             rect.render_widget(list, chunks[1]);
-        });
-    }
 
-    Ok(())
+            match events_reciever.recv().expect("foobar") {
+                Tick => {
+                    println!("gotta tick!")
+                }
+                Resize => {
+                    println!("gotta resize")
+                }
+                Input(event) => {
+                    println!("gotta keyboard event {:?}", event)
+                }
+            }
+        })?;
+    }
 }
