ba3a34f32b9a434e70dce4694941533705f0939f
Grant Powell <email@grantjamespowell.com>
2021-10-08 08:53:50 -0400

    Add wrapping methods to rows and cols in tic tac toe

--- games/tic_tac_toe/src/board.rs
+++ games/tic_tac_toe/src/board.rs
@@ -5,7 +5,55 @@ use std::collections::HashMap;
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct Col(u8);
 
+impl Default for Col {
+    fn default() -> Self {
+        Self(0)
+    }
+}
+
+impl Into<u8> for Col {
+    fn into(self) -> u8 {
+        self.0
+    }
+}
+
 impl Col {
+    /// Returns the next column, wrapping back to 0 after 2
+    ///
+    /// ```
+    /// use tic_tac_toe::Col;
+    ///
+    /// assert_eq!(Col::new(0).next(), Col::new(1));
+    /// assert_eq!(Col::new(1).next(), Col::new(2));
+    /// assert_eq!(Col::new(2).next(), Col::new(0));
+    /// ```
+    pub fn next(&self) -> Self {
+        Self(match self.0 {
+            0 => 1,
+            1 => 2,
+            2 => 0,
+            _ => panic!("invalid index"),
+        })
+    }
+
+    /// Returns the previous column, wrapping back to 2 after 0
+    ///
+    /// ```
+    /// use tic_tac_toe::Col;
+    ///
+    /// assert_eq!(Col::new(2).previous(), Col::new(1));
+    /// assert_eq!(Col::new(1).previous(), Col::new(0));
+    /// assert_eq!(Col::new(0).previous(), Col::new(2));
+    /// ```
+    pub fn previous(&self) -> Self {
+        Self(match self.0 {
+            2 => 1,
+            1 => 0,
+            0 => 2,
+            _ => panic!("invalid index"),
+        })
+    }
+
     /// Construct a new `Col` (see `Col::try_new` for a non panicking version)
     ///
     /// # Panics
@@ -19,6 +67,7 @@ impl Col {
     pub fn new(n: usize) -> Self {
         Self::try_new(n).expect("Invalid index, n must be within 0..=2")
     }
+
 
     /// Try to construct a `Col`, returning None if n is out of bounds
     ///
@@ -38,7 +87,55 @@ impl Col {
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct Row(u8);
 
+impl Default for Row {
+    fn default() -> Self {
+        Self(0)
+    }
+}
+
+impl Into<u8> for Row {
+    fn into(self) -> u8 {
+        self.0
+    }
+}
+
 impl Row {
+    /// Returns the next column, wrapping back to 0 after 2
+    ///
+    /// ```
+    /// use tic_tac_toe::Row;
+    ///
+    /// assert_eq!(Row::new(0).next(), Row::new(1));
+    /// assert_eq!(Row::new(1).next(), Row::new(2));
+    /// assert_eq!(Row::new(2).next(), Row::new(0));
+    /// ```
+    pub fn next(&self) -> Self {
+        Self(match self.0 {
+            0 => 1,
+            1 => 2,
+            2 => 0,
+            _ => panic!("invalid index"),
+        })
+    }
+
+    /// Returns the previous column, wrapping back to 2 after 0
+    ///
+    /// ```
+    /// use tic_tac_toe::Row;
+    ///
+    /// assert_eq!(Row::new(2).previous(), Row::new(1));
+    /// assert_eq!(Row::new(1).previous(), Row::new(0));
+    /// assert_eq!(Row::new(0).previous(), Row::new(2));
+    /// ```
+    pub fn previous(&self) -> Self {
+        Self(match self.0 {
+            2 => 1,
+            1 => 0,
+            0 => 2,
+            _ => panic!("invalid index"),
+        })
+    }
+
     /// Construct a new Row (see `Row::try_new` for a non panicking version)
     ///
     /// # Panics
