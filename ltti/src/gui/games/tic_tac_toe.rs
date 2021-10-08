21c7faed7b6af2d1eb8e568322186023070e19f7
Grant Powell <email@grantjamespowell.com>
2021-10-08 01:03:25 -0400

    Start rendering some blocks in tic tac toe

--- ltti/src/gui/games/tic_tac_toe.rs
+++ ltti/src/gui/games/tic_tac_toe.rs
@@ -2,7 +2,6 @@
 // use tic_tac_toe::{Board, Col, Row, Settings, Status};
 // use tui::backend::Backend;
 // use tui::layout::Constraint;
-// use tui::style::{Color, Style};
 // use tui::terminal::Frame;
 // use tui::text::Span;
 // use tui::widgets::{self, Block, BorderType, Borders, Cell, Table, Widget};
@@ -10,8 +9,11 @@
 use crate::gui::{GameUserInterface, UserInterfaceState};
 use lttcore::Play;
 use tic_tac_toe::TicTacToe;
+
 use tui::backend::Backend;
-use tui::layout::Rect;
+use tui::layout::{Alignment, Constraint::*, Direction::*, Layout, Margin, Rect};
+use tui::style::{Color::*, Style};
+use tui::widgets::{Block, BorderType::*, Borders};
 use tui::Frame;
 
 #[derive(Default)]
@@ -32,69 +34,42 @@ impl<B: Backend> GameUserInterface<B> for TicTacToe {
         action_request: &<Self as Play>::ActionRequest,
         submit: impl FnOnce(<Self as Play>::Action),
     ) {
+        let board = Block::default()
+            .title("Tic Tac Toe")
+            .borders(Borders::ALL)
+            .border_type(Thick);
+
+        let inner_rect = board.inner(rect).inner(&Margin {
+            horizontal: 2,
+            vertical: 1,
+        });
+
+        frame.render_widget(board, rect);
+
+        let cols = Layout::default()
+            .direction(Horizontal)
+            .constraints([Percentage(33), Percentage(33), Percentage(33)])
+            .split(inner_rect);
+
+        let squares = cols.iter().map(|&column_rect| {
+            Layout::default()
+                .direction(Vertical)
+                .constraints([Percentage(33), Percentage(33), Percentage(33)])
+                .split(column_rect)
+        });
+
+        for (col_num, col) in squares.into_iter().enumerate() {
+            for (row_num, square) in col.into_iter().rev().enumerate() {
+                let x = format!("({},{})", col_num, row_num);
+
+                let block = Block::default()
+                    .title(x)
+                    .borders(Borders::ALL)
+                    .border_style(Style::default().fg(White))
+                    .style(Style::default().bg(Black));
+
+                frame.render_widget(block, square);
+            }
+        }
     }
 }
-
-// pub fn render(settings: &Settings, board: &Board) -> impl Widget {
-//     let table = (0..=2)
-//         .rev()
-//         .map(|col| {
-//             (0..=2).map(move |row| {
-//                 let marker = board.at((col, row));
-//                 make_cell((col, row), marker)
-//             })
-//         })
-//         .map(|cells| widgets::Row::new(cells).height(5).bottom_margin(1))
-//         .collect::<Vec<widgets::Row>>();
-//
-//     Table::new(table)
-//         .style(Style::default().fg(Color::White).bg(Color::Black))
-//         .widths(&[
-//             Constraint::Length(10),
-//             Constraint::Length(10),
-//             Constraint::Length(10),
-//         ])
-//         .column_spacing(3)
-//         .block(
-//             Block::default()
-//                 .borders(Borders::ALL)
-//                 .style(Style::default().fg(Color::White))
-//                 .title("Tic Tac Toe")
-//                 .border_type(BorderType::Plain),
-//         )
-// }
-//
-// fn make_cell((col, row): (usize, usize), marker: Option<Marker>) -> Cell<'static> {
-//     let marker_text = match marker {
-//         Some(X) => "X",
-//         Some(O) => "O",
-//         None => " ",
-//     };
-//
-//     let contents = format!(
-//         concat!("{}\n\n", "  {}  \n\n", "{:?}",),
-//         col_and_row_to_phone_pad((col, row)),
-//         marker_text,
-//         (col, row)
-//     );
-//
-//     let cell = Cell::from(contents);
-//
-//     match marker {
-//         Some(X) => cell.style(Style::default().bg(Color::LightRed)),
-//         Some(O) => cell.style(Style::default().bg(Color::LightBlue)),
-//         None => cell.style(Style::default().bg(Color::DarkGray)),
-//     }
-// }
-//
-// fn col_and_row_to_phone_pad(position: (usize, usize)) -> usize {
-//     4
-// }
-//
-// // fn status_bar(status: &Status) -> impl Widget {
-// //     todo!()
-// // }
-// //
-// // fn board(board: &Board) -> impl Widget {
-// //     todo!()
-// // }
