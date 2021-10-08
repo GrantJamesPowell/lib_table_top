c8275720688ec94b469d2913ac6c29940c4347cd
Grant Powell <email@grantjamespowell.com>
2021-10-07 22:27:35 -0400

    Attempt 1 at a tic tac toe gui

--- ltti/src/gui/games/tic_tac_toe.rs
+++ ltti/src/gui/games/tic_tac_toe.rs
@@ -1,6 +1,72 @@
-use tic_tac_toe::{Board, Status};
-use tui::widgets::Widget;
+use tic_tac_toe::Marker::{self, *};
+use tic_tac_toe::{Board, Col, Row, Settings, Status};
+use tui::backend::Backend;
+use tui::layout::Constraint;
+use tui::style::{Color, Style};
+use tui::terminal::Frame;
+use tui::text::Span;
+use tui::widgets::{self, Block, BorderType, Borders, Cell, Table, Widget};
 
-fn render_game(board: &Board) -> impl Widget {
-    todo!()
+pub fn render(settings: &Settings, board: &Board) -> impl Widget {
+    let table = (0..=2)
+        .rev()
+        .map(|col| {
+            (0..=2).map(move |row| {
+                let marker = board.at((col, row));
+                make_cell((col, row), marker)
+            })
+        })
+        .map(|cells| widgets::Row::new(cells).height(5).bottom_margin(1))
+        .collect::<Vec<widgets::Row>>();
+
+    Table::new(table)
+        .style(Style::default().fg(Color::White).bg(Color::Black))
+        .widths(&[
+            Constraint::Length(10),
+            Constraint::Length(10),
+            Constraint::Length(10),
+        ])
+        .column_spacing(3)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .style(Style::default().fg(Color::White))
+                .title("Tic Tac Toe")
+                .border_type(BorderType::Plain),
+        )
 }
+
+fn make_cell((col, row): (usize, usize), marker: Option<Marker>) -> Cell<'static> {
+    let marker_text = match marker {
+        Some(X) => "X",
+        Some(O) => "O",
+        None => " ",
+    };
+
+    let contents = format!(
+        concat!("{}\n\n", "  {}  \n\n", "{:?}",),
+        col_and_row_to_phone_pad((col, row)),
+        marker_text,
+        (col, row)
+    );
+
+    let cell = Cell::from(contents);
+
+    match marker {
+        Some(X) => cell.style(Style::default().bg(Color::LightRed)),
+        Some(O) => cell.style(Style::default().bg(Color::LightBlue)),
+        None => cell.style(Style::default().bg(Color::DarkGray)),
+    }
+}
+
+fn col_and_row_to_phone_pad(position: (usize, usize)) -> usize {
+    4
+}
+
+// fn status_bar(status: &Status) -> impl Widget {
+//     todo!()
+// }
+//
+// fn board(board: &Board) -> impl Widget {
+//     todo!()
+// }
--- ltti/src/main.rs
+++ ltti/src/main.rs
@@ -15,6 +15,7 @@ use std::{io, sync::mpsc, thread, time::Duration};
 mod gui;
 
 use gui::common::{footer, layout};
+use gui::games::tic_tac_toe;
 use gui::tick::{background_terminal_events_and_ticks, Event::*};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -35,6 +36,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         terminal.draw(|rect| {
             let chunks = layout().split(rect.size());
 
+            let board = ::tic_tac_toe::Board::from_ints([[0, 1, 2], [1, 0, 1], [2, 2, 0]]);
+
+            use lttcore::player::p;
+            let settings = ::tic_tac_toe::Settings::new([p(1), p(2)]);
+
+            rect.render_widget(tic_tac_toe::render(&settings, &board), chunks[1]);
             rect.render_widget(footer(), chunks[2]);
 
             match events_reciever.recv().expect("foobar") {
