078f7799b8748edf89723f7ce6216f59fea26d94
Grant Powell <email@grantjamespowell.com>
2021-10-12 12:25:11 -0400

    Add in the `Card` abstraction to lttcore::deck

---
+++ lttcore/src/common/deck/card.rs
@@ -0,0 +1,56 @@
+use crate::common::deck::{Color, Rank, Suit};
+use std::fmt;
+
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd, Eq, Hash, Ord)]
+pub struct Card(Rank, Suit);
+
+impl From<(Rank, Suit)> for Card {
+    fn from((rank, suit): (Rank, Suit)) -> Self {
+        Card::new(rank, suit)
+    }
+}
+
+impl fmt::Display for Card {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?} of {:?}", self.rank(), self.suit())
+    }
+}
+
+impl Card {
+    pub fn new(rank: Rank, suit: Suit) -> Self {
+        Self(rank, suit)
+    }
+
+    pub fn color(&self) -> Color {
+        self.1.color()
+    }
+    pub fn suit(&self) -> Suit {
+        self.1
+    }
+
+    pub fn rank(&self) -> Rank {
+        self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::common::deck::Rank::*;
+    use crate::common::deck::Suit::*;
+
+    #[test]
+    fn test_display() {
+        let test_cases = [
+            (Card::new(Ace, Spades), "Ace of Spades"),
+            (Card::new(King, Hearts), "King of Hearts"),
+            (Card::new(Ten, Clubs), "Ten of Clubs"),
+            (Card::new(Two, Diamonds), "Two of Diamonds"),
+        ];
+
+        for (card, expected) in test_cases.iter() {
+            let displayed = format!("{}", card);
+            assert_eq!(displayed, *expected);
+        }
+    }
+}
