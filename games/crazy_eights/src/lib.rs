3fa193ace3de56915baaaa382646f1846c6fb7d6
Grant Powell <email@grantjamespowell.com>
2021-10-14 15:17:49 -0400

    Start to skeleton out what the spectator view looks like
    This is actually pretty tricky, I think I'll have more clarity after
    figuring out what settings I'm going to support.

--- games/crazy_eights/src/lib.rs
+++ games/crazy_eights/src/lib.rs
@@ -9,12 +9,22 @@ pub use settings::Settings;
 pub use spectator_view::SpectatorView;
 
 use lttcore::{
-    common::deck::{Card, Color, Rank, Suit},
-    play::{ActionResponse, GameAdvance},
-    Play, Player,
+    common::deck::{Card, Suit},
+    Player,
 };
-use thiserror::Error;
+// use thiserror::Error;
 
-struct CrazyEights {
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum Action {
+    /// Draw a card from the draw pile. Reshuffles the deck if there are no cards remaining in the
+    /// draw pile. If there are no cards in the draw pile or discard pile, this is a no-op.
+    Draw,
+    /// Play a card from your hand
+    Play(Card),
+    /// Play and eight, and select the next suit
+    PlayEight(Card, Suit),
+}
+
+pub struct CrazyEights {
     resigned: Vec<Player>,
 }
--- games/crazy_eights/src/spectator_view.rs
+++ games/crazy_eights/src/spectator_view.rs
@@ -1,1 +1,52 @@
-pub struct SpectatorView {}
+use std::collections::HashMap;
+use std::error::Error;
+
+use lttcore::common::deck::{Card, Rank, Suit};
+use lttcore::{Player, View};
+
+use crate::Action;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct BoardInfo {
+    current_suit_and_rank: (Suit, Rank),
+    draw_pile_num_remaining_cards: usize,
+    top_card: Card,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct SpectatorView {
+    whose_turn: Player,
+    discard_pile: Vec<Card>,
+    player_card_count: HashMap<Player, usize>,
+    board_info: BoardInfo,
+}
+
+impl SpectatorView {
+    fn valid_actions(&self, hand: &[Card]) -> impl Iterator<Item = Action> {
+        // todo!()
+        [].into_iter()
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum PlayerAction {
+    PlayCard {
+        card: Card,
+        player: Player,
+        player_hand_size_after_playing_card: usize,
+    },
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Update {
+    player_action: PlayerAction,
+    board_info: BoardInfo,
+}
+
+impl View for SpectatorView {
+    type Update = Update;
+
+    fn update(&mut self, _update: &Self::Update) -> Result<(), Box<dyn Error>> {
+        Ok(())
+    }
+}
