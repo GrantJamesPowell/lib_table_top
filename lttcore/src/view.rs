3d9d628b3aad86892975fd427cff9def829a058c
Grant Powell <email@grantjamespowell.com>
2021-10-23 18:54:44 -0400

    Require that views implement `{De}serialize`

--- lttcore/src/view.rs
+++ lttcore/src/view.rs
@@ -1,19 +1,20 @@
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use std::error::Error;
 use std::fmt::Debug;
 
-pub trait View: Clone + Debug {
-    type Update: Clone + Debug;
+pub trait View: Clone + Debug + Serialize + DeserializeOwned {
+    type Update: Clone + Debug + Serialize + DeserializeOwned;
 
     fn update(&mut self, _update: &Self::Update) -> Result<(), Box<dyn Error>> {
         Ok(())
     }
 }
 
-#[derive(Debug, Default, Clone, PartialEq, Eq)]
-pub struct NoSecretPlayerInformation {}
+#[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct NoSecretPlayerInformation;
 
-#[derive(Debug, Default, Clone, PartialEq, Eq)]
-pub struct NoSecretPlayerInformationUpdate {}
+#[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct NoSecretPlayerInformationUpdate;
 
 impl View for NoSecretPlayerInformation {
     type Update = NoSecretPlayerInformationUpdate;
