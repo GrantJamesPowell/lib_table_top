224e18cb28ca70d13df04086dfc563eaa6502036
Grant Powell <email@grantjamespowell.com>
2021-10-27 12:43:26 -0400

    Move turn to it's own file

--- lttcore/src/game_runner.rs
+++ lttcore/src/game_runner.rs
@@ -5,11 +5,9 @@ use std::collections::HashMap;
 use std::sync::Arc;
 
 use crate::play::{ActionResponse, GameAdvance};
-use crate::{NumberOfPlayers, Play, Player, PlayerSet, Seed};
+use crate::{NumberOfPlayers, Play, Player, Seed, Turn};
 
-use thiserror::Error;
-
-type Actions<T> = SmallVec<[(Player, ActionResponse<<T as Play>::Action>); 2]>;
+pub type Actions<T> = SmallVec<[(Player, ActionResponse<<T as Play>::Action>); 2]>;
 
 #[derive(Builder, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[builder(setter(into, strip_option), build_fn(skip))]
@@ -32,83 +30,6 @@ pub struct GameRunner<T: Play> {
 #[serde(bound = "")]
 pub struct HistoryEvent<T: Play> {
     actions: Actions<T>,
-}
-
-#[derive(Debug, Clone)]
-pub struct Turn<T: Play> {
-    turn_num: u64,
-    action_requests: PlayerSet,
-    actions: Actions<T>,
-}
-
-impl<T: Play> Turn<T> {
-    pub fn number(&self) -> u64 {
-        self.turn_num
-    }
-
-    pub fn pending_action_requests(&self) -> PlayerSet {
-        self.action_requests
-            .players()
-            .filter(|player| {
-                self.actions
-                    .binary_search_by_key(&player, |(p, _)| &*p)
-                    .is_err()
-            })
-            .collect()
-    }
-
-    /// Add an action to the turn
-    ///
-    /// # Panics
-    ///
-    /// This panics if the `Player` isn't in the turn
-    /// ```should_panic
-    /// use lttcore::examples::{GuessTheNumber, guess_the_number::Guess};
-    /// use lttcore::{Player, GameRunnerBuilder};
-    ///
-    /// let player: Player = 255.into();
-    /// let game = GameRunnerBuilder::<GuessTheNumber>::default().build().unwrap();
-    /// let mut turn = game.turn().unwrap();
-    ///
-    /// let guess: Guess = 42.into();
-    /// turn.add_action(player, guess);
-    /// ```
-    pub fn add_action(
-        &mut self,
-        player: impl Into<Player>,
-        action_response: impl Into<ActionResponse<<T as Play>::Action>>,
-    ) {
-        let player = player.into();
-        let action_response = action_response.into();
-
-        assert!(
-            self.action_requests.contains(player),
-            "{:?} was added to turn {:?}, but player isn't in the turn",
-            player,
-            self.turn_num
-        );
-
-        match self.actions.binary_search_by_key(&player, |(p, _)| *p) {
-            Ok(existing_action_index) => {
-                self.actions[existing_action_index] = (player, action_response);
-            }
-            Err(index) => {
-                self.actions.insert(index, (player, action_response));
-            }
-        }
-    }
-
-    pub fn is_ready_to_submit(&self) -> bool {
-        self.pending_action_requests().is_empty()
-    }
-}
-
-#[derive(Error, Debug, Clone, Copy)]
-pub enum SubmitError {
-    #[error("can't add action for player that was not requested")]
-    InvalidPlayer,
-    #[error("can't submit turn with invalid actions")]
-    InvalidTurn,
 }
 
 impl<T: Play> GameRunner<T> {
