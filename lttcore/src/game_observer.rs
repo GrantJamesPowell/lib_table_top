b4bf4ecc09a4bbf94c4644b46e1bd2f8e4c43ce2
Grant Powell <email@grantjamespowell.com>
2021-10-29 11:04:40 -0400

    Implement the GameObserver

--- lttcore/src/game_observer.rs
+++ lttcore/src/game_observer.rs
@@ -1,1 +1,17 @@
-pub struct GameObserver;
+use crate::{Observe, Observer, Play};
+
+pub struct GameObserver<T: Play> {
+    turn_num: u64,
+    settings: <T as Play>::Settings,
+    public_info: <T as Play>::PublicInfo,
+}
+
+impl<T: Play> Observe<T> for GameObserver<T> {
+    fn observe(&self) -> Observer<'_, T> {
+        Observer {
+            turn_num: self.turn_num,
+            settings: &self.settings,
+            public_info: &self.public_info,
+        }
+    }
+}
