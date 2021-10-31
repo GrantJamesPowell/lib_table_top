f50d71f218b51e9eaa8bdac10765a0801fee0ac5
Grant Powell <email@grantjamespowell.com>
2021-10-31 12:12:28 -0400

    Remove weird vestigal next_player from player struct

--- lttcore/src/player.rs
+++ lttcore/src/player.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
-use crate::common::direction::LeftOrRight::{self, *};
-use crate::{NumberOfPlayers, PlayerSet};
+
+
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
 pub struct Player(u8);
@@ -31,74 +31,5 @@ impl Player {
 impl From<u8> for Player {
     fn from(n: u8) -> Self {
         Self(n)
-    }
-}
-
-impl Player {
-    /// Returns the "next" player, taking into account direction, number of players, and
-    /// resignations. Will return `None` if all players are resigned, including the current player.
-    /// ```
-    /// use lttcore::{Player, PlayerSet, common::direction::LeftOrRight::*};
-    ///
-    /// let player: Player = 1.into();
-    /// let num_players = 3.try_into().unwrap();
-    /// let mut resignations: PlayerSet = Default::default();
-    ///
-    /// // The 0th player is to the left of the first player
-    /// assert_eq!(
-    ///   player.next_player_for_direction(Left, num_players, &resignations),
-    ///   Some(0.into())
-    /// );
-    ///
-    /// // The 2nd Player is to the right of the first player
-    /// ```
-    ///
-    /// # Panics
-    ///
-    /// This will panic if the player is outside of the `num_players` range.
-    /// This will likely represent  a logic bug in game code.
-    ///
-    /// ```should_panic
-    /// use lttcore::{Player, PlayerSet, common::direction::LeftOrRight::*};
-    /// let player: Player = 42.into();
-    /// let num_players = 3.try_into().unwrap();
-    ///
-    /// player.next_player_for_direction(Left, num_players, &Default::default());
-    /// ```
-    pub fn next_player_for_direction(
-        &self,
-        direction: LeftOrRight,
-        num_players: NumberOfPlayers,
-        resignations: &PlayerSet,
-    ) -> Option<Player> {
-        assert!(
-            num_players.includes_player(*self),
-            "current_player is not in num_players"
-        );
-
-        let next = |p: Player| -> Player {
-            match direction {
-                Left => match p.as_u8() {
-                    0 => (num_players.get() - 1).into(),
-                    n => (n - 1).into(),
-                },
-                Right => match p.as_u8() {
-                    n if n == (num_players.get() - 1) => 0.into(),
-                    n => (n + 1).into(),
-                },
-            }
-        };
-
-        let mut curr: Player = next(*self);
-
-        loop {
-            if curr == *self {
-                return Some(*self).filter(|&p| !resignations.contains(p));
-            } else if !resignations.contains(curr) {
-                return Some(curr);
-            } else {
-                curr = next(curr)
-            }
-        }
     }
 }
