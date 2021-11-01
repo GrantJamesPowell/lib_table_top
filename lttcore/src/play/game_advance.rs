cca34cc97e03118008c28f8e2ed4fd969f7feeb7
Grant Powell <email@grantjamespowell.com>
2021-11-01 16:10:54 -0400

    `EnumeratedGameAdvance` into `Game{Observer/Player}Update`

--- lttcore/src/play/game_advance.rs
+++ lttcore/src/play/game_advance.rs
@@ -1,5 +1,7 @@
 use super::{DebugMsgs, Play, PlayerSecretInfoUpdates, View};
-use crate::{PlayerSet, TurnNum};
+use crate::pov::{ObserverUpdate, PlayerUpdate};
+use crate::{Player, PlayerSet, TurnNum};
+use std::borrow::Cow;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct GameAdvance<T: Play> {
@@ -13,4 +15,31 @@ pub struct GameAdvance<T: Play> {
 pub struct EnumeratedGameAdvance<T: Play> {
     pub turn_num: TurnNum,
     pub game_advance: GameAdvance<T>,
+}
+
+impl<T: Play> EnumeratedGameAdvance<T> {
+    fn observer_update(&self) -> ObserverUpdate<'_, T> {
+        ObserverUpdate {
+            turn_num: self.turn_num,
+            action_requests: self.game_advance.next_players_input_needed,
+            public_info_update: Cow::Borrowed(&self.game_advance.public_info_update),
+        }
+    }
+
+    fn player_update(&self, player: impl Into<Player>) -> PlayerUpdate<'_, T> {
+        let player = player.into();
+        let observer_update = self.observer_update();
+        let secret_info_update = self
+            .game_advance
+            .player_secret_info_updates
+            .iter()
+            .filter(|(p, _)| *p == player)
+            .next()
+            .map(|(_, update)| Cow::Borrowed(update));
+
+        PlayerUpdate {
+            secret_info_update,
+            observer_update,
+        }
+    }
 }
