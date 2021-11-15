6fa04e7c154998eb3f09cecbc84ed3a37eb27f04
Grant Powell <email@grantjamespowell.com>
2021-11-15 13:31:33 -0500

    Require more of `Play` to be `Hash`

--- lttcore/src/play/mode.rs
+++ lttcore/src/play/mode.rs
@@ -1,12 +1,11 @@
 use crate::play::LttSettings;
 use crate::Play;
-use serde::{Deserialize, Serialize};
-use std::sync::Arc;
+
+
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Mode<T: Play> {
     name: &'static str,
-    #[serde(skip)]
     _phantom: std::marker::PhantomData<T>,
 }
 
@@ -24,39 +23,56 @@ impl<T: Play> Mode<T> {
         self.name
     }
 
-    pub fn settings(&self) -> &Arc<T::Settings> {
+    pub fn settings(&self) -> &'static T::Settings {
         <T::Settings as LttSettings>::game_modes()
             .get(self.name)
             .unwrap()
     }
 }
 
-// Todo: Fix Mode Serialization
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use crate::examples::GuessTheNumber;
-//     use serde_json::json;
-//
-//     #[test]
-//     fn test_serde_mode() {
-//         // Using the try_new constructor
-//         assert!(Mode::<GuessTheNumber>::try_new("players-1-range-1-10").is_some());
-//         assert!(Mode::<GuessTheNumber>::try_new("players-2-range-1-10").is_some());
-//         assert!(Mode::<GuessTheNumber>::try_new("players-3-range-1-10").is_some());
-//         assert!(Mode::<GuessTheNumber>::try_new("players-4-range-1-10").is_some());
-//         assert!(Mode::<GuessTheNumber>::try_new("foo bar baz").is_none());
-//
-//         // Serde
-//         let mode: Mode<GuessTheNumber> = Mode::try_new("players-1-range-1-10").unwrap();
-//         let serialized = serde_json::to_value(&mode).unwrap();
-//         assert_eq!(serialized, json!({"name": "players-1-range-1-10"}));
-//         let deserialized: Mode<GuessTheNumber> = serde_json::from_value(serialized).unwrap();
-//         assert_eq!(deserialized, mode);
-//
-//         // it's an error to try to deserialize an invalid game
-//         assert!(
-//             serde_json::from_value::<Mode<GuessTheNumber>>(json!({"name": "foo bar baz"})).is_err()
-//         );
-//     }
-// }
+impl<T: Play> std::ops::Deref for Mode<T> {
+    type Target = T::Settings;
+
+    fn deref(&self) -> &Self::Target {
+        self.settings()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::examples::GuessTheNumber;
+
+    #[test]
+    fn test_mode_deref() {
+        let mode = Mode::<GuessTheNumber>::try_new("players-2-range-1-10").unwrap();
+
+        assert_eq!(mode.range(), 1..=10);
+        assert_eq!(mode.number_of_players(), 2.try_into().unwrap());
+    }
+
+    // TODO: Fix Mode Serialization
+    // use serde_json::json;
+
+    // #[test]
+    // fn test_serde_mode() {
+    //     // Using the try_new constructor
+    //     assert!(Mode::<GuessTheNumber>::try_new("players-1-range-1-10").is_some());
+    //     assert!(Mode::<GuessTheNumber>::try_new("players-2-range-1-10").is_some());
+    //     assert!(Mode::<GuessTheNumber>::try_new("players-3-range-1-10").is_some());
+    //     assert!(Mode::<GuessTheNumber>::try_new("players-4-range-1-10").is_some());
+    //     assert!(Mode::<GuessTheNumber>::try_new("foo bar baz").is_none());
+
+    //     // Serde
+    //     let mode: Mode<GuessTheNumber> = Mode::try_new("players-1-range-1-10").unwrap();
+    //     let serialized = serde_json::to_value(&mode).unwrap();
+    //     assert_eq!(serialized, json!({"name": "players-1-range-1-10"}));
+    //     let deserialized: Mode<GuessTheNumber> = serde_json::from_value(serialized).unwrap();
+    //     assert_eq!(deserialized, mode);
+
+    //     // it's an error to try to deserialize an invalid game
+    //     assert!(
+    //         serde_json::from_value::<Mode<GuessTheNumber>>(json!({"name": "foo bar baz"})).is_err()
+    //     );
+    // }
+}
