cf19ca3027870e251f9a5f01f366dddb4a3d71e0
Grant Powell <email@grantjamespowell.com>
2021-11-17 20:10:22 -0500

    Support `FromStr` for `Token`

--- lttnetworking/src/token.rs
+++ lttnetworking/src/token.rs
@@ -1,4 +1,13 @@
+pub use hex::{FromHex, FromHexError};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Token(#[serde(with = "hex")] [u8; 32]);
+
+impl std::str::FromStr for Token {
+    type Err = FromHexError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        <[u8; 32]>::from_hex(s).map(Token)
+    }
+}
