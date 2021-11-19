3a3692f4caba1249f070a54ebef3906909d8642d
Grant Powell <email@grantjamespowell.com>
2021-11-19 13:45:01 -0500

    Introduce `Wrapper` types for bots
    I think some games will follow the pattern of having "phases" of play.
    The `Bot::run` interface is pretty low level so it would be nice if we
    could provide higher level traits that define a more game specific
    interface.
    
    The example in this commit is the `FishFightBot` trait. It defines two
    seperate methods for `setup_board` and `guess`. It also removes the need
    to setup the `type Game = FishFight` and wrapping returns in
    `FishFight::Action`.
    
    The other thing that I think this interface does well is that it creates
    seperate views into the game depending on the "phase" the game is in.
    `FishFightGuessPov` is specific to the game whereas `PlayerPov` is a
    little harder to deal with directly

---
+++ lttcore/src/examples/fish_fight/bot.rs
@@ -0,0 +1,47 @@
+use super::{Action, Board, FishFight, FishPositions, Position, PublicInfo, Settings};
+use crate::bots::Bot;
+use crate::pov::PlayerPov;
+use crate::utilities::PlayerIndexedData as PID;
+use crate::{Play, Player, TurnNum};
+
+pub trait FishFightBot {
+    fn setup_board(settings: &Settings, rng: &mut impl rand::Rng) -> FishPositions;
+    fn guess(pov: FishFightGuessPov<'_>, rng: &mut impl rand::Rng) -> Position;
+}
+
+pub struct FishFightGuessPov<'a> {
+    pub turn_num: TurnNum,
+    pub player: Player,
+    pub settings: &'a Settings,
+    pub boards: &'a PID<Board>,
+    pub player_fish_positions: &'a FishPositions,
+}
+
+pub struct FishFightBotWrapper<T: FishFightBot>(T);
+
+impl<T: FishFightBot> Bot for FishFightBotWrapper<T> {
+    type Game = FishFight;
+
+    fn run(
+        player_pov: &PlayerPov<'_, Self::Game>,
+        rng: &mut impl rand::Rng,
+    ) -> <Self::Game as Play>::Action {
+        match player_pov.public_info {
+            PublicInfo::Setup => {
+                let position = T::setup_board(player_pov.settings, rng);
+                Action::Setup(position)
+            }
+            PublicInfo::Playing { boards } => {
+                let pov = FishFightGuessPov {
+                    turn_num: player_pov.turn_num,
+                    player: player_pov.player,
+                    settings: player_pov.settings,
+                    player_fish_positions: &player_pov.secret_info.fish_positions,
+                    boards,
+                };
+                let position = T::guess(pov, rng);
+                Action::Guess(position)
+            }
+        }
+    }
+}
