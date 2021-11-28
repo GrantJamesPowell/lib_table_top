f5ec6e792f379f1cac0abb0c904862f4c185a709
Grant Powell <email@grantjamespowell.com>
2021-11-27 23:33:33 -0500

    Inital work on the `RawStorage` && `Storage` traits

---
+++ lttstorage/src/raw_storage.rs
@@ -0,0 +1,52 @@
+use super::storage::{MetaData, SettingsType, StorageError};
+use async_trait::async_trait;
+use bytes::Bytes;
+use lttcore::{
+    encoder::Encoding,
+    id::{GameId, SettingsId},
+};
+use lttcore::{utilities::PlayerIndexedData, Seed, TurnNum};
+
+pub struct RawCustomSettings {
+    pub name: Option<String>,
+    pub bytes: Bytes,
+    pub game_type: String,
+}
+
+pub struct RawHistoryEvent {
+    turn_num: TurnNum,
+    actions: PlayerIndexedData<Bytes>,
+}
+
+pub struct RawGameProgression {
+    seed: Seed,
+    settings: SettingsType,
+    turn_num: TurnNum,
+    initial_state: Bytes,
+    history_events: Vec<RawHistoryEvent>,
+}
+
+#[async_trait]
+pub trait RawStorage: Send + Sync + 'static {
+    fn encoding(&self) -> Encoding;
+
+    async fn read_raw_custom_settings(
+        &self,
+        id: SettingsId,
+    ) -> Result<(SettingsId, RawCustomSettings, MetaData), StorageError>;
+
+    async fn read_raw_game_progression(
+        &self,
+        id: GameId,
+    ) -> Result<(GameId, RawGameProgression, MetaData), StorageError>;
+
+    async fn write_raw_custom_settings(
+        &self,
+        insert: (SettingsId, RawCustomSettings, MetaData),
+    ) -> Result<(), StorageError>;
+
+    async fn write_raw_game_progression(
+        &self,
+        insert: (GameId, RawGameProgression, MetaData),
+    ) -> Result<(), StorageError>;
+}
