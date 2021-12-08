fb09bf2e167ed4f25e656434ec4ad768440fe108
Grant Powell <email@grantjamespowell.com>
2021-12-08 15:28:58 -0500

    Suppoer `ObserverPov` and `PlayerPov` directly from `GameProgression`

--- lttcore/src/pov/game_progression/support_povs.rs
+++ lttcore/src/pov/game_progression/support_povs.rs
@@ -1,8 +1,31 @@
 use super::GameProgression;
-use crate::play::{Play, Player, SettingsPtr};
 use crate::pov::{observer::GameObserver, player::GamePlayer};
+use crate::{
+    play::{Play, Player, SettingsPtr},
+    pov::{observer::ObserverPov, player::PlayerPov},
+};
 
 impl<T: Play> GameProgression<T> {
+    pub fn observer_pov(&self) -> ObserverPov<'_, T> {
+        ObserverPov {
+            turn_num: self.turn_num,
+            settings: self.settings(),
+            public_info: self.public_info(),
+        }
+    }
+
+    pub fn player_pov(&self, player: impl Into<Player>) -> PlayerPov<'_, T> {
+        let player = player.into();
+
+        PlayerPov {
+            player,
+            turn_num: self.turn_num,
+            settings: self.settings(),
+            secret_info: self.player_secret_info(player),
+            public_info: self.public_info(),
+        }
+    }
+
     pub fn game_observer(&self) -> GameObserver<T> {
         GameObserver {
             turn_num: self.turn_num(),
