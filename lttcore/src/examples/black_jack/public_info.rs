bedbda26d35e51acfe5cfa5b79031558eef3876c
Grant Powell <email@grantjamespowell.com>
2021-12-09 15:40:14 -0500

    A very basic public info action flow for the game
    I'm trying to balance how much info to pass through the `Phase` field of
    the `PublicInfo` it's tricky because any logic that gets written into
    `BlackJackBot` needs to be replicated in any other bot implementation.
    It would be nice to have a dead simple `Bot` implementation for anything
    wanting to play black jack. The other half of that is wanting to avoid
    denormalizing data. It would be really nice if I could avoid having to
    make sure multiple sources of data are in sync

--- lttcore/src/examples/black_jack/public_info.rs
+++ lttcore/src/examples/black_jack/public_info.rs
@@ -1,19 +1,62 @@
-use crate::play::{Score, TurnNum, View};
+use crate::common::deck::Card;
+use crate::play::{Player, Score, TurnNum, View};
 use crate::utilities::PlayerIndexedData as PID;
 use serde::{Deserialize, Serialize};
+use smallvec::SmallVec;
 use std::borrow::Cow;
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum Phase {
+    Bet,
+    PlayHand(Player, usize),
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct PublicInfo {
     pub statuses: PID<PlayerStatus>,
+    pub phase: Phase,
+    pub hands: PID<SmallVec<[Hand; 1]>>,
+}
+
+impl PublicInfo {
+    pub fn dealer_card_showing(&self) -> Option<Card> {
+        todo!()
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub enum PublicInfoUpdate {}
+pub struct Hand {
+    pub bet: u32,
+    pub cards: SmallVec<[Card; 4]>,
+}
+
+impl Hand {
+    pub fn has_taken_additional_cards(&self) -> bool {
+        todo!()
+    }
+
+    pub fn is_able_to_split(&self) -> bool {
+        todo!()
+    }
+
+    pub fn is_able_to_double_down(&self) -> bool {
+        todo!()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum PublicInfoUpdate {
+    EndHand {
+        dealer_hand: Hand,
+        status_updates: PID<PlayerStatus>,
+    },
+    AddHand(Player, Hand),
+    UpdateHand(Player, usize, Hand),
+    UpdateStatus(PID<PlayerStatus>),
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum PlayerStatus {
-    HitScoreLimit { turn: TurnNum },
     Resigned { turn: TurnNum, chips: u32 },
     InPlay { chips: u32 },
     Busted { turn: TurnNum },
@@ -21,12 +64,37 @@ pub enum PlayerStatus {
 
 impl Score for PublicInfo {
     fn score(&self) -> Option<PID<i64>> {
-        todo!()
+        use PlayerStatus::*;
+
+        Some(self.statuses.map(|status| match status {
+            Resigned { chips, .. } | InPlay { chips, .. } => *chips as i64,
+            Busted { .. } => 0,
+        }))
     }
 }
 
 impl View for PublicInfo {
     type Update = PublicInfoUpdate;
 
-    fn update(&mut self, _update: Cow<'_, Self::Update>) {}
+    fn update(&mut self, update: Cow<'_, Self::Update>) {
+        use PublicInfoUpdate::*;
+
+        match update.into_owned() {
+            EndHand {
+                dealer_hand: _,
+                status_updates,
+            } => {
+                self.statuses.extend(status_updates);
+            }
+            AddHand(player, hand) => {
+                self.hands[player].push(hand);
+            }
+            UpdateHand(player, idx, hand) => {
+                self.hands[player][idx] = hand;
+            }
+            UpdateStatus(status_updates) => {
+                self.statuses.extend(status_updates);
+            }
+        }
+    }
 }
