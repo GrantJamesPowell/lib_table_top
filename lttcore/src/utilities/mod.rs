31ab140481b3929b8960a97b5693a1b0aac8196f
Grant Powell <email@grantjamespowell.com>
2022-07-29 19:05:16 -0400

    Remove 'stat'

--- lttcore/src/utilities/mod.rs
+++ lttcore/src/utilities/mod.rs
@@ -4,7 +4,6 @@ mod bit_array_256;
 mod player_indexed_data;
 mod player_item_collector;
 mod player_set;
-pub mod stat;
 
 pub use bit_array_256::BitArray256;
 pub use player_indexed_data::PlayerIndexedData;
